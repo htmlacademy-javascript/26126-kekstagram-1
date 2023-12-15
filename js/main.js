b2a3618f2d854e47befa0e1fdf8c9b3f4513e6e0
Katarios2015 <prichal7@bk.ru>
2023-12-15 08:24:10 +0300

    Отрисуй меня полностью, правки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 import './util.js';
-import './data.js';
+import {createPhotoDescriptions} from './data.js';
+import {createGallery} from './mini-pictures.js';
 import './const.js';
 import './mini-pictures.js';
+
+const photoDescriptions = createPhotoDescriptions();
+
+createGallery(photoDescriptions);
--- js/mini-pictures.js
+++ js/mini-pictures.js
@@ -1,19 +1,21 @@
-import {createPhotoDescriptions} from './data.js';
-
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 const picturesContainer = document.querySelector('.pictures');
 const picturesFragment = document.createDocumentFragment();
 
-const photoDescriptions = createPhotoDescriptions();
 
-photoDescriptions.slice().forEach(({url, likes, comments})=> {
-  pictureTemplate.querySelector('.picture__img').src = url;
-  pictureTemplate.querySelector('.picture__likes').textContent = likes;
-  pictureTemplate.querySelector('.picture__comments').textContent = comments.length;
-  const miniaturePicture = pictureTemplate.cloneNode(true);
-  picturesFragment.appendChild(miniaturePicture);
-});
+const createGallery = (photosArray) => {
+  photosArray.slice().forEach(({url, likes, comments})=> {
+    pictureTemplate.querySelector('.picture__img').src = url;
+    pictureTemplate.querySelector('.picture__likes').textContent = likes;
+    pictureTemplate.querySelector('.picture__comments').textContent = comments.length;
+    const miniaturePicture = pictureTemplate.cloneNode(true);
+    picturesFragment.appendChild(miniaturePicture);
+  });
+  const gallery = picturesContainer.appendChild(picturesFragment);
 
-picturesContainer.appendChild(picturesFragment);
+  return gallery;
+};
+
+export {createGallery};
