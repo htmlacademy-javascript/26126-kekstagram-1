055caaa3c2869333b6497bd4a19f183ce0ac0294
Katarios2015 <prichal7@bk.ru>
2023-12-20 15:50:48 +0300

    Открывается и закрывается (часть 2)

--- js/full-picture.js
+++ js/full-picture.js
@@ -1,5 +1,6 @@
 import {picturesContainer} from './mini-pictures.js';
 import {isEscapeKey} from './util.js';
+import {COMMENT_STEP} from './const.js';
 
 const pageBody = document.querySelector('body');
 const bigPictureModal = document.querySelector('.big-picture');
@@ -10,8 +11,9 @@ const commentTemplate = document.querySelector('#comment')
 const commentsFragment = document.createDocumentFragment();
 const commentsContainer = bigPictureModal.querySelector('.social__comments');
 
-const visibleCommentsCount = bigPictureModal.querySelector('.social__comment-count');
+const visibleCommentsCount = bigPictureModal.querySelector('.comments-count-showed');
 const totalCommentsCount = bigPictureModal.querySelector('.comments-count');
+const commentsLoader = bigPictureModal.querySelector('.social__comments-loader');
 
 const exitBtn = bigPictureModal.querySelector('#picture-cancel');
 
@@ -23,16 +25,30 @@ const fillBigPhoto = (emptyBigPhoto, currentObject) => {
     .querySelector('img').src = currentObject.url;
 };
 
+let renderedCommentsFirstIndex = 0;
+let newRenderedCommentsLastIndex = COMMENT_STEP;
+let onCommentsLoaderClick = () => {};
+
 const createCommentsList = (currentObject) => {
-  currentObject.comments.slice(0, 5).forEach(({avatar, message, name}) => {
-    const comment = commentTemplate.cloneNode(true);
-    comment.querySelector('.social__picture').src = avatar;
-    comment.querySelector('.social__picture').alt = name;
-    comment.querySelector('.social__text').textContent = message;
+  const comments = currentObject.comments;
+  const commentsCount = comments.length;
+  totalCommentsCount.textContent = commentsCount;
 
-    commentsFragment.appendChild(comment);
-  });
+  visibleCommentsCount.textContent = Math.min(commentsCount,newRenderedCommentsLastIndex);
 
+  if (newRenderedCommentsLastIndex >= commentsCount){
+    commentsLoader.classList.add('hidden');
+  }
+
+  comments.slice(renderedCommentsFirstIndex, newRenderedCommentsLastIndex)
+    .forEach(({avatar, message, name}) => {
+      const comment = commentTemplate.cloneNode(true);
+      comment.querySelector('.social__picture').src = avatar;
+      comment.querySelector('.social__picture').alt = name;
+      comment.querySelector('.social__text').textContent = message;
+
+      commentsFragment.appendChild(comment);
+    });
   commentsContainer.appendChild(commentsFragment);
 };
 
@@ -57,9 +73,13 @@ function closeFullPhoto () {
   pageBody.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
   exitBtn.removeEventListener('click', onExitBtnClick);
+  renderedCommentsFirstIndex = 0;
+  newRenderedCommentsLastIndex = COMMENT_STEP;
+  commentsLoader.classList.remove('hidden');
+  commentsLoader.removeEventListener('click', onCommentsLoaderClick);
 }
 
-const getFullPhotoOpenHandler = (photos) => {
+const initFullPhotoOpenHandler = (photos) => {
   picturesContainer.addEventListener('click', (evt) => {
     const currentPicture = evt.target.closest('.picture');
     if(currentPicture) {
@@ -69,17 +89,20 @@ const getFullPhotoOpenHandler = (photos) => {
       bigPictureModal.classList.remove('hidden');
       pageBody.classList.add('modal-open');
 
-      visibleCommentsCount.classList.add('hidden');
-      totalCommentsCount.classList.add('hidden');
-
       document.addEventListener('keydown', onDocumentKeydown);
       exitBtn.addEventListener('click', onExitBtnClick);
 
       fillBigPhoto(bigPictureModal, pictureObject);
       createCommentsList(pictureObject);
+      onCommentsLoaderClick = () => {
+        renderedCommentsFirstIndex = newRenderedCommentsLastIndex;
+        newRenderedCommentsLastIndex += COMMENT_STEP;
+        createCommentsList(pictureObject);
+      };
+      commentsLoader.addEventListener('click', onCommentsLoaderClick);
     }
   });
 };
 
 
-export {getFullPhotoOpenHandler};
+export {initFullPhotoOpenHandler};
